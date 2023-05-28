d4c0a844f09644730086aaefd62a98c00cf4953a
Aleksandr <azelen04@mail.ru>
2023-05-28 17:02:48 +0300

    Made config for Rubik's Cube

---
+++ Rubik/RubikConfig.h
@@ -0,0 +1,34 @@
+#ifndef RUBIKCUBE_RUBIKCONFIG_H
+#define RUBIKCUBE_RUBIKCONFIG_H
+
+#define GL_SILENCE_DEPRECATION
+#include <OpenGL/gl.h>
+#include <OpenGL/OpenGL.h>
+#include <GLUT/glut.h>
+#include <fstream>
+
+#define GLEW_STATIC
+using namespace std;
+
+ifstream inputStream("input.txt");
+ofstream outputStream("output.txt");
+
+enum Colors {
+    yellow = 0xFFFF00,
+    white = 0xFFFFFF,
+    blue = 0x0000FF,
+    green = 0x00FF00,
+    orange = 0xFF6600,
+    red = 0xFF0000,
+};
+
+const int ROTATE_SPEED_STEP = 10, ROTATE_START_VALUE = 30;
+GLfloat CUBE_SIZE = 12;
+
+// Projection of the angle of rotation  on the axis
+int xRot = 25, yRot = -45;
+
+// Translation by Z axis
+GLfloat translateZ = -35;
+
+#endif //RUBIKCUBE_RUBIKCONFIG_H
